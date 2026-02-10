51c76edd57566e6f37443f0342767a7cc5f57590
violet <ianpk2001@gmail.com>
2026-02-10 10:19:33 -0600

    set up flight state code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <Arduino.h>
+#include <future>
 #include "AllSensors.h"
 
 // GPS on Serial2, LSM CS=10, BMP CS=9
 AllSensors sensors(Serial2, 9600, 10, 9);
+enum State {
+    PRE_LAUNCH,
+    ASCENT,
+    DESCENT,
+    LANDED
+};
+
+State flightState = PRE_LAUNCH;
+
+bool launchDetect(&sensor);
 
 void setup() {
     Serial.begin(115200);
@@ -12,5 +23,31 @@ void setup() {
 }
 
 void loop() {
-
+    switch (flightState) {
+        case PRE_LAUNCH:
+            if (launchDetect(&sensors)) {
+                flightState = ASCENT;
+                break;
+            } else {
+                break;
+            }
+        case ASCENT:
+            if (apogeeDetect(&sensors)) {
+                flightState = DESCENT;
+                break;
+            } else {
+                break;
+            }
+        case DESCENT:
+            if (landingDetect(&sensors)) {
+                flightState = LANDED;
+                break;
+            } else {
+                break;
+            }
+        case LANDED:
+            break;
+        default:
+            
+    }
 }
